4e5b7ae7fcb3f68b5b32cbb26dc6d2c2e03716a0
turboaaa <turboaaa@gmail.com>
2019-04-16 09:59:31 -0400

    Update pass.sh
    typo

--- pass.sh
+++ pass.sh
@@ -9,7 +9,7 @@ tmpfile=`date +%F%s`_pass
 echo -e "$2\n$pass" | net rpc password "$1" -U "$domain"\\"$admin" -S "$server" &> /tmp/"$tmpfile"
 result=$(</tmp/"$tmpfile")
 ##Check for errors
-grep -qi 'NT_STATUS_ACCOUNT_DISABLED' <<< $ersult && report='Account disabled.'
+grep -qi 'NT_STATUS_ACCOUNT_DISABLED' <<< $result && report='Account disabled.'
 grep -qi 'NT_STATUS_LOGON_FAILURE' <<< $result && report='Unable to log into server.'
 grep -qi 'NT_STATUS_UNSUCCESSFUL' <<< $result && report='Unable to find server.'
 grep -qi 'NT_STATUS_NOT_SUPPORTED' <<< $result && report='Unable to log into server.'
